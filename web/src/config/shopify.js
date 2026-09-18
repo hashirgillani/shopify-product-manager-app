9bab7a410c00fd8b2cc80b467133ce1246487dea
hashirgillani <hashirgillani0@gmail.com>
2026-09-19 02:16:19 +0500

    chore: remove frontend code from app.js

--- web/src/config/shopify.js
+++ web/src/config/shopify.js
@@ -4,11 +4,8 @@ import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlit
 import { restResources } from "@shopify/shopify-api/rest/admin/2024-10";
 import { DB_PATH } from "../constants.js";
 
-// The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
-// See the ensureBilling helper to learn more about billing in this template.
 const billingConfig = {
   "My Shopify One-Time Charge": {
-    // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
     amount: 5.0,
     currencyCode: "USD",
     interval: BillingInterval.OneTime,
@@ -24,7 +21,7 @@ const shopify = shopifyApp({
       lineItemBilling: true,
       unstable_managedPricingSupport: true,
     },
-    billing: undefined, // or replace with billingConfig above to enable example billing
+    billing: undefined, 
   },
   auth: {
     path: "/api/auth",
@@ -33,7 +30,6 @@ const shopify = shopifyApp({
   webhooks: {
     path: "/api/webhooks",
   },
-  // This should be replaced with your preferred storage strategy
   sessionStorage: new SQLiteSessionStorage(DB_PATH),
 });
 
--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,11 @@
 // @ts-check
+import dotenv from"dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+
+dotenv.config({
+    path: './.env'
+})
 
 const PORT = parseInt(
   process.env.BACKEND_PORT || process.env.PORT || "3000",
